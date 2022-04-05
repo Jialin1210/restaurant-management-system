3e85467a8c1efb73543d5f63a90f877c1731695f
carrol song <songc@dickinson.edu>
2022-04-05 18:41:30 -0400

    server updated

--- server.py
+++ server.py
@@ -304,7 +304,7 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute("SELECT name FROM tes
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
